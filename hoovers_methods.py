1f9aa1f13b5074eae75196404a343cf170ec4218
Matt Nicholson <matthew.n.nicholson1@gmail.com>
2017-05-02 16:22:34 -0500

    fixed features and added classification

--- hoovers_methods.py
+++ hoovers_methods.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 27 12:27:34 2017
+Created on Wed Apr 26 12:27:34 2017
 
 implementing Adam Hoover's method 
 @author: Matt
@@ -9,7 +9,8 @@ import csv
 import numpy as np
 import pandas as pd
 import scipy.ndimage
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt 
+
 import os
 
 path="" #../../MotifCounter/
@@ -40,19 +41,35 @@ def readData(path):
     print("done reading in the data")
     return X
 
+def gauss(t,n,sigma): #In the paper n=31 sigma=10
+    num=np.exp(- t**2 / (2*sigma**2))
+    print(num)
+    
+    denom=sum([np.exp((-(number-n)**2)/ (2*sigma**2)) for number in range(n+1)]) 
+    print(denom)
+    
+    print(num/denom)
+    return num/denom
+    
+
 def smooth(x): #TODO: figure out good params for the smoothing
     #So there are 15 obs/sec and I think the hoover paper smoothed minute by minute
     #not really about sigma or window size, or end behavior     
     y=x    
     
+
+    
     for col in [0, 1, 2, 3, 5, 6]: # skip the time index
-        y[:,col]=scipy.ndimage.filters.gaussian_filter1d(x[:,col], 10) #this is from the paper
+        y[:,col]=scipy.ndimage.filters.gaussian_filter1d(x[:,col], 10)#maybe replace this with the other helper function
+        
+        
+        #this is from the paper
         #might need to have this be a per thing basis
 
     smoothed_df=pd.DataFrame(y)
     smoothed_df.to_csv(subj+"_smoothed.csv")    
     
-    print("oooohh weeeee I smoothed some data")
+    print("I smoothed some data")
     return y
     
 def energyGeneration(x): 
@@ -88,14 +105,13 @@ def hooverSegmentation(energy): #I'm pretty sure this is working, but there isn'
     plt.show()    
     
     peak_dictionary={"time":[],"value":[]}
-      
-    
+        
     print(energy.shape[0])
-    while t < energy.shape[0]: #forget the while loop until I can detect 1 peak correctly
+    while t < energy.shape[0]: 
 #        print("the t value is currently:", t)
 #        print("the energy value is currently:", energy["Energy"].iloc[t])
         thresh1=energy["Energy"].iloc[t]
-        thresh2=thresh1*2
+        thresh2=thresh1*1.5 #originally this is 2
         
 #        print("the thresholds are",thresh1,thresh2)
         
@@ -106,7 +122,7 @@ def hooverSegmentation(energy): #I'm pretty sure this is working, but there isn'
                 #reset the thresholds
                 print("reseting the threshold at index",local_t)
                 thresh1=energy["Energy"].iloc[local_t]
-                thresh2=thresh1*2
+                thresh2=thresh1*1.5
                 
         t=local_t
        
@@ -129,8 +145,6 @@ def hooverSegmentation(energy): #I'm pretty sure this is working, but there isn'
         peak_dictionary["time"].append(hmm)
         peak_dictionary["value"].append(local_peak)
         
-
-        
         #maybe update the start of the buffer here?
         start_segment=t        
         
@@ -139,8 +153,7 @@ def hooverSegmentation(energy): #I'm pretty sure this is working, but there isn'
     peaks=pd.DataFrame(peak_dictionary)
     peaks.to_csv(path+subj+"_peaks.csv")
     
-    
-    return peak_dictionary
+    return peaks
     
 def featureGeneration(smooth, peaks):
     #do some stuff to shape according to the peaks and then have a for loop
@@ -148,25 +161,32 @@ def featureGeneration(smooth, peaks):
     
     #TODO: this first part
     #smooth + peaks -> data
+    number_segments=peaks.shape[0]+1    
     
+    #probably should add what the segment is
+    features=pd.DataFrame(np.zeros((number_segments,4)), columns=["LinearAcc","Manipulation", "WristRoll","WristRollRegularity"])
     
-    features=pd.DataFrame(np.zeros((number of segments,4)), names["LinearAcc","Manipulation", "WristRoll","WristRollRegularity"] )
-    
+    start_t=0    
     iter=0
-    for segment in data:
-        features["Manipulation"].iloc[iter]=manipulationFeature(segement)
+    for t in peaks["time"]:
+        segment=smooth.loc[start_t:t]
+        features["Manipulation"].iloc[iter]=manipulationFeature(segment)
         features["LinearAcc"].iloc[iter]=linearAccelerationFeature(segment)
         features["WristRoll"].iloc[iter]=wristRollFeature(segment)
         features["WristRollRegularity"].iloc[iter]=wristRollRegularityFeature(segment)
+        print("finshed with segment",iter )
         iter+=1
-            
+        start_t=t
+
+    features.to_csv(path+subj+"_features.csv")
+    
     return features
     
 def manipulationFeature(segment):
     num=[abs(number) for number in segment["Angular_Velocity_x"]]+[abs(number) for number in segment["Angular_Velocity_y"]]+[abs(number) for number in segment["Angular_Velocity_z"]]
     denom=[abs(number) for number in segment["Linear_Accel_x"]]+[abs(number) for number in segment["Linear_Accel_y"]]+[abs(number) for number in segment["Linear_Accel_z"]]
     
-    manip=1/(len(segment))*sum(num/denom) # I really hope this is elementwise
+    manip=1/(len(segment))*sum(np.divide(num,denom)) # I really hope this is elementwise
     #also hope this is in degrees per second
     
     return manip
@@ -174,17 +194,18 @@ def manipulationFeature(segment):
 def linearAccelerationFeature(segment):
     linacc=sum([abs(number) for number in segment["Linear_Accel_y"]])+sum([abs(number) for number in segment["Linear_Accel_z"]])
     linacc=1/(len(segment)) *  linacc #this might have to be shape[0] insteas of len
-   
-   return energy
+    return linacc
   
 def wristRollFeature(segment):
     #double check to make sure this is supposed to be y
-    return 1/(len(segment)) * [abs(number- np.average(segement["Angular_Velocity_y"])) for number in segement["Angular_Velocity_y"]]
+    wr=1/(len(segment)) * sum([abs(number - np.average(segment["Angular_Velocity_y"])) for number in segment["Angular_Velocity_y"]])
+    
+    return wr
     
 def wristRollRegularityFeature(segment):
     #I don't get the whole +8 thing
     #TODO: fix this with the + 8 seconds
-    return 1/(len(segment))*[1 if abs(number) > 10 for number in segment["Angular_Velocity_y"]]
+    return 1/(len(segment))*sum([1 for number in segment["Angular_Velocity_y"] if abs(number) > 10 ])
 
 
 def classification(feats):
@@ -203,9 +224,11 @@ if __name__ == "__main__":
     if not os.path.exists(path+subj+"_peaks.csv"):
         peaks=hooverSegmentation(energy)
     else:
-        peaks=pd.read_csv(subj+"_peaks.csv", names=["time",'value'],index_col=0, header=0)
-        
-    featureGeneration(smoothed,peaks)
-#    features
+        peaks=pd.read_csv(path+subj+"_peaks.csv", names=["time",'value'],index_col=0, header=0)
+      
+    if not os.path.exists(path+subj+"_peaks.csv"):
+        features=featureGeneration(smoothed,peaks)
+    else:
+        features=pd.read_csv(path+subj+"_features.csv",index_col=0, header=0,names=["LinearAcc","Manipulation", "WristRoll","WristRollRegularity"])
     
     print("yay done with main!")
